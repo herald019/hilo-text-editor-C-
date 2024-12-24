5158b137656b3457f0c6127d0a8e589ae823309f
Harry Herald <131639717+herald019@users.noreply.github.com>
2024-12-24 23:24:56 +0530

    Add files via upload
    functions for most key press on terminal window

--- hilo.c
+++ hilo.c
@@ -11,12 +11,17 @@
 /*** defines ***/
 #define CTRL_KEY(k) ((k) & 0x1f) // used to mimic the terminal to deal with ctrl button
 #define HILO_VERSION "0.0.1"
-
-enum editorKey {
+enum editorKey
+{
     ARROW_LEFT = 1000,
-    ARROW_RIGHT,  // rest are incremented from 1000
-    ARROW_UP, // ie, 1001, 1002, 1003 respectively :)
-    ARROW_DOWN 
+    ARROW_RIGHT, // rest are incremented from 1000
+    ARROW_UP,    // ie, 1001, 1002, 1003 respectively :)
+    ARROW_DOWN,
+    PAGE_UP,
+    PAGE_DOWN,
+    HOME_KEY,
+    END_KEY,
+    DEL_KEY
 };
 
 /*** data ***/
@@ -75,25 +80,53 @@ int editorReadKey()
     if (c == '\x1b')
     {
         char seq[3];
-        if (read(STDIN_FILENO, &seq[0], 1) != 1)
-            return '\x1b';
-        if (read(STDIN_FILENO, &seq[1], 1) != 1)
-            return '\x1b';
+        if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+        if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
         if (seq[0] == '[')
         {
-            switch (seq[1])
+            if (seq[1] >= '0' && seq[1] <= '9')
             {
-            case 'A': return ARROW_UP;
-            case 'B': return ARROW_DOWN;
-            case 'C': return ARROW_RIGHT;
-            case 'D': return ARROW_LEFT;
+                if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+                if (seq[2] == '~')
+                {
+                    switch (seq[1])
+                    {
+                    case '1': return HOME_KEY;
+                    case '3': return DEL_KEY;
+                    case '4': return END_KEY;
+                    case '5': return PAGE_UP;
+                    case '6': return PAGE_DOWN;
+                    case '7': return HOME_KEY;
+                    case '8': return END_KEY;
+                    }
+                }
+            }
+            else
+            {
+                switch (seq[1])
+                {
+                case 'A': return ARROW_UP;
+                case 'B': return ARROW_DOWN;
+                case 'C': return ARROW_RIGHT;
+                case 'D': return ARROW_LEFT;
+                case 'H': return HOME_KEY;
+                case 'F': return END_KEY;
+                }
+            }
+        } 
+        else if (seq[0] == 'O') 
+        {
+            switch (seq[1]) 
+            {
+                case 'H': return HOME_KEY;
+                case 'F': return END_KEY;
             }
         }
         return '\x1b';
     }
     else
     {
-
         return c;
     }
 }
@@ -227,16 +260,28 @@ void editorMoveCursor(int key)
     switch (key)
     {
     case ARROW_LEFT:
-        E.cx--;
+        if (E.cx != 0)
+        {
+            E.cx--;
+        }
         break;
     case ARROW_RIGHT:
-        E.cx++;
+        if (E.cx != E.screencols - 1)
+        {
+            E.cx++;
+        }
         break;
     case ARROW_UP:
-        E.cy--;
+        if (E.cy != 0)
+        {
+            E.cy--;
+        }
         break;
     case ARROW_DOWN:
-        E.cy++;
+        if (E.cy != E.screenrows - 1)
+        {
+            E.cy++;
+        }
         break;
     }
 }
@@ -252,6 +297,22 @@ void editorProcessKeypress()
         write(STDOUT_FILENO, "\x1b[H", 3);
         exit(0);
         break;
+
+    case HOME_KEY:
+      E.cx = 0;
+      break;
+    case END_KEY:
+      E.cx = E.screencols - 1;
+      break;
+
+    case PAGE_UP:
+    case PAGE_DOWN:
+    {
+        int times = E.screenrows; // however many rows on the screen
+        while (times--)
+            editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+    }
+    break;
 
     case ARROW_UP:
     case ARROW_DOWN:
